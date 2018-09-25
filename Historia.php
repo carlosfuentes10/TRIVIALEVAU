8e24689af8c6c74f1faf18c40e7a48779c503a66
Administrator <Administrator@pc.localdomain>
2018-09-25 19:18:09 +0200

    CARPETA CSS Y JS METIDAS

--- Historia.php
+++ Historia.php
@@ -2,11 +2,21 @@
     <head>
         <meta charset="UTF-8">
         <title>HISTORIA</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+         <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
     </head>
-    <body>
+    <body style="background-color: yellow">
+         <div class="container">
+            <div class ="row">
+                <div class="col-12">
+                    <h2 class="text-center" style="color:black;">HISTORIA</h2> 
+                </div>
+            </div>
+         </div>
+        
         <?php
         // put your code here
         ?>
     </body>
+    <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+   
 </html>
