1287a4b9de644c6fd7c34c88b96a4f9ef97199a8
Administrator <Administrator@pc.localdomain>
2018-09-25 18:31:14 +0200

    CARPETA CSS Y JS METIDAS

---
+++ Historia.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HISTORIA</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
